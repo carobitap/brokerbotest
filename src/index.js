61484e2879d4352c1ea29ca4e72a2196dddedcb7
Caro <carolina.garcia@agavesoft.com.mx>
2021-01-13 14:52:40 -0600

    API test for bitso with node

---
+++ src/index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const logger = require('morgan');
+const bitso = require('bitso-node-api');
+var app = express();
+
+/*let client = new bitso {
+    key: ''
+    secret: ''
+    authorizeMovements: true
+}*/
+
+//Middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json()); 
+
+//Balance
+// Define your request
+var key = "Bitso api key";
+var secret = "Bitso api secret";
+var httpMethod = "GET";
+var request_path="/v3/balance/"
+var json_payload={};
+
+// Create the signature
+var nonce = new Date().getTime();
+var message = nonce+httpMethod+request_path;
+var payload = JSON.stringify(json_payload)
+if (httpMethod == "POST")
+  message += payload;
+var crypto = require('crypto');
+var signature = crypto.createHmac('sha256', secret).update(message).digest('hex');
+
+// Build the auth header
+var authHeader = "Bitso "+key+":" +nonce+":"+signature;
+
+// Send request
+var options = {
+  host: 'api.bitso.com',
+  path: request_path,
+  method: httpMethod,
+  headers: {
+    'Authorization': authHeader, //Bitso <key>:<nonce>:<signature>
+    'Content-Type': 'application/json'
+  }
+};
+
+// Send request
+var http = require('https');
+var req = http.request(options, function(res) {
+  console.log(`statusCode: ${res.statusCode}`);
+  console.log(`statusMessage: ${res.statusMessage}`);
+  res.on('data', function (chunk) {
+    console.log(`body: ${chunk}`);
+  });
+});
+req.on('error', (error) => {
+  console.error(error)
+})
+if (httpMethod == "POST") {
+  req.write(payload);
+}
+req.end();
+
+/*Buy
+client.private.placeOrder({
+    book: bitso.exchanges.BTC_MXN,
+    side: 'buy',
+    type: 'market',
+    minor: '10000',
+  })
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+*/
+
+//Starting server
+app.listen(3000, function(err){ 
+    if (err) console.log(err); 
+    console.log(`Server on port ${3000}`); 
+});
---
+++ src/tests.js
@@ -0,0 +1,113 @@
+const bitso = require("./BitsoAPI");
+const chalk = require("chalk");
+const bitsoUrl = process.env.bitsoUrl;
+const bitsoApiKey = process.env.bitsoApiKey;
+const bitsoApiSecret = process.env.bitsoApiSecret;
+
+const bitsoAPI = new bitso(
+    bitsoApiKey,
+    bitsoApiSecret,
+    bitsoUrl,
+    "/api/v3"
+);
+
+function displayError(Exception) {
+    console.log("Failed");
+    if (Exception.response !== undefined) {
+        console.log(
+            `${Exception.response.status} : ${
+                Exception.response.statusText
+            } ${JSON.stringify(Exception.response.data)}`
+        );
+    } else {
+        console.log(Exception);
+    }
+}
+
+function checkIfSuccessful(test, data) {
+    if (data !== undefined && data.success) {
+        console.log(chalk.green(`${test} Test passed`));
+    } else {
+        console.log(chalk.red(`${test} Test failed`));
+        console.log(JSON.stringify(data));
+    }
+    return data.success;
+}
+
+async function testBalances() {
+    preTestingMessage("Balances");
+    try {
+        let response = await client.getBalances();
+        if (checkIfSuccessful("Balances", response)) return response.payload;
+    } catch (Exception) {
+        displayError(Exception);
+    }
+}
+
+async function testwithdrawals(currency, amount, address) {
+    preTestingMessage("withdrawals", currency, amount, address);
+    try {
+        let response = await client.withdrawCrypto(currency, amount, address);
+        if (checkIfSuccessful("withdrawals", response)) return response.payload;
+    } catch (Exception) {
+        displayError(Exception);
+    }
+}
+
+async function testOrders(book, side, amount, type) {
+    preTestingMessage("Orders", book, side, amount, type);
+    try {
+        let response = await client.placeOrder(book, side, amount, type);
+        if (checkIfSuccessful("Orders", response)) return response.payload;
+    } catch (Exception) {
+        displayError(Exception);
+    }
+}
+
+async function testAvailableBooks() {
+    preTestingMessage("Available books");
+    try {
+        let response = await client.getAvailableBooks();
+        if (checkIfSuccessful("Available books", response))
+            return response.payload;
+    } catch (Exception) {
+        displayError(Exception);
+    }
+}
+
+async function testTicker(book) {
+    preTestingMessage("Ticker", book);
+    try {
+        let response = await client.getCurrentTicker(book);
+        if (checkIfSuccessful("Ticker", response)) return response.payload;
+    } catch (Exception) {
+        displayError(Exception);
+    }
+}
+
+async function testGetTrades(book) {
+    preTestingMessage("Get trades", book);
+    try {
+        let response = await client.getTrades(book);
+        if (checkIfSuccessful("Get trades", response)) return response.payload;
+    } catch (Exception) {
+        displayError(Exception);
+    }
+}
+function preTestingMessage(test, ...rest) {
+    console.log(chalk.blue(`TESTING ${test} ENDPOINT`));
+    console.log(chalk.blue(`USING PARAMETERS: ${rest}`));
+}
+
+// Simply tests the functions no actual integrity checks
+async function basicTesting() {
+    await testAvailableBooks();
+    await testTicker("btc_mxn");
+    await testGetTrades("btc_mxn");
+    await testOrders("eth_mxn", "buy", "1.0", "market");
+    await testOrders("eth_mxn", "sell", "1.0", "market");
+    await testWithdrawals("btc", "0.001", "15YB8xZ4GhHCHRZXvgmSFAzEiDosbkDyoo");
+    await testBalances();
+}
+
+basicTesting();
